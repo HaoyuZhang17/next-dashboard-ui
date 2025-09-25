1fe9318a9c4d8b72d109d1e5652d61a183af4c3d
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2025-09-24 20:43:57 -0600

    create page by different user

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return <div className="h-screen flex">
+      {/* left */}
+      <div className='w-1/6'>l</div>
+      {/* right */}
+      <div className='w-5/6'>r</div>
+    </div>
+  
+    
+  }
