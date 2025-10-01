d80cacc4beb753bdb3786a84d0780c67e92f9a83
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2025-09-30 18:18:05 -0600

    list

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,7 +1,72 @@
 import Pagination from "@/components/Pagination"
+import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
 import Image from "next/image"
+
+type Teacher = {
+  id:number;
+  teacherId:string;
+  email?:string;
+  photo:string;
+  phone:string;
+  subjects:string[];
+  classes:string[];
+  address:string;
+}
+const columns = [
+  {
+    header:"Info", 
+    accessor:"info"
+  },
+  {
+    header:"Teacher ID", 
+    accessor:"teacherId",
+    // 默认隐藏；md及以上：显示为表格单元格
+    className:"hidden md:table-cell",
+  },
+  {
+    header:"Subjects", 
+    accessor:"subjects",
+    // 默认隐藏；md及以上：显示为表格单元格
+    className:"hidden md:table-cell",
+  },
+  {
+    header:"Classes", 
+    accessor:"classes",
+    // 默认隐藏；md及以上：显示为表格单元格
+    className:"hidden md:table-cell",
+  },
+  {
+    header:"Phone", 
+    accessor:"phone",
+    // 默认隐藏；md及以上：显示为表格单元格
+    className:"hidden lg:table-cell",
+  },
+
+  {
+    header:"Address", 
+    accessor:"address",
+    // 默认隐藏；md及以上：显示为表格单元格
+    className:"hidden lg:table-cell",
+  },
+
+  {
+    header:"Actions", 
+    accessor:"actions",
+
+  },
+
+]
 const TeacherListPage = () => {
+
+  const renderRow = (item:Teacher) => {
+    <tr>
+      <td>
+        {/* object-cover：图片保持比例，尽可能铺满容器 */}
+        <Image src={item.phone} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"></Image>
+      </td>
+    </tr>
+  }
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* top */}
@@ -26,7 +91,7 @@ const TeacherListPage = () => {
       </div>
 
       {/* list */}
-
+      <Table columns={columns}/>
       {/* pagination */}
 
       <Pagination />
