97d2cce697200caf38bef6702a6fd9965badf983
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2025-09-30 22:38:17 -0600

    create teacher list

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,11 +1,14 @@
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
+import { role, teachersData } from "@/lib/data"
 import Image from "next/image"
+import Link from "next/link"
 
 type Teacher = {
   id:number;
   teacherId:string;
+  name:string;
   email?:string;
   photo:string;
   phone:string;
@@ -59,14 +62,40 @@ const columns = [
 ]
 const TeacherListPage = () => {
 
-  const renderRow = (item:Teacher) => {
-    <tr>
-      <td>
+  const renderRow = (item:Teacher) => (
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+      <td className="flex items-center gap-4 p-4">
         {/* object-cover：图片保持比例，尽可能铺满容器 */}
-        <Image src={item.phone} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"></Image>
+        <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"></Image>
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      {/* className="hidden md:table-cell" join是js里的一个数组的方法：array.join(separator)；把每一项拼接成一个字符串，用separator连接 */}
+      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      {/* ACTION */}
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+                <Image src="/delete.png" alt="" width={16} height={16} />
+              </button>
+            )}
+          
+        </div>
       </td>
     </tr>
-  }
+  );
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* top */}
@@ -91,7 +120,7 @@ const TeacherListPage = () => {
       </div>
 
       {/* list */}
-      <Table columns={columns}/>
+      <Table columns={columns} renderRow={renderRow} data={teachersData}/>
       {/* pagination */}
 
       <Pagination />
