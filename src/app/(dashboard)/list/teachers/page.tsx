8d8dcf3db2d2852173c9d177798343d002b6b113
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2025-10-03 00:02:26 -0600

    添加了formmodal组件，引入teacher的page的按钮

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -87,9 +88,11 @@ const TeacherListPage = () => {
             </button>
           </Link>
             {role === "admin" && (
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                <Image src="/delete.png" alt="" width={16} height={16} />
-              </button>
+              // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+              //   <Image src="/delete.png" alt="" width={16} height={16} />
+              // </button>
+              // 引用组件FormModal
+              <FormModal table="teacher" type="create"/>
             )}
           
         </div>
