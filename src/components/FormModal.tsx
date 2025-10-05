2b634125db36526608b540c513f2b4fc20b2ac8e
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2025-10-04 21:33:01 -0600

    finish form design

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 const FormModal = ({table,type,data,id}:
   {
     table:
@@ -36,7 +37,7 @@ const FormModal = ({table,type,data,id}:
         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delect</button>
       </form>
     ) : (
-      "create or update form"
+      <TeacherForm type="create"/> 
     );
   }
 
